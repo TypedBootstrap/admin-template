da335f8e9699293eea50e3bcc02b1f0294d79cd8
Andrew Dyer <andrewdyer@outlook.com>
2020-03-22 14:20:51 +0000

    :hammer: Added active status based on current location

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -12,61 +12,84 @@ import {
     UncontrolledDropdown
 } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import menu from 'config/menu';
 
 /**
  * Sidebar
  */
-const Sidebar = ({ sidebarToggled }) => (
-    <div className={classnames('Sidebar', { toggled: sidebarToggled })}>
-        <Nav navbar>
-            {menu.map((item, i) => {
-                if (item.heading) {
-                    return <NavbarText key={i}>{item.heading}</NavbarText>;
-                } else if (!item.subMenu) {
-                    return (
-                        <NavItem key={i}>
-                            <NavLink tag={Link} to={item.path}>
-                                <FontAwesomeIcon icon={item.icon} fixedWidth />
-                                <span>{item.name}</span>
-                            </NavLink>
-                        </NavItem>
-                    );
-                } else if (item.subMenu) {
-                    return (
-                        <UncontrolledDropdown key={i} nav inNavbar>
-                            <DropdownToggle nav>
-                                <FontAwesomeIcon icon={item.icon} fixedWidth />
-                                <span>{item.name}</span>
-                            </DropdownToggle>
-                            <DropdownMenu>
-                                {item.subMenu.map((item, i) => {
-                                    if (item.heading) {
+const Sidebar = ({ location, sidebarToggled }) => {
+    const isRouteActive = paths => {
+        paths = Array.isArray(paths) ? paths : [paths];
+        if (paths.indexOf(location.pathname) > -1) {
+            return true;
+        }
+
+        return false;
+    };
+
+    const getSubMenuRoutes = item => {
+        return item.subMenu.map(({ path }) => path);
+    };
+
+    return (
+        <div className={classnames('Sidebar', { toggled: sidebarToggled })}>
+            <Nav navbar>
+                {menu.map((item, i) => {
+                    if (item.heading) {
+                        return <NavbarText key={i}>{item.heading}</NavbarText>;
+                    } else if (!item.subMenu) {
+                        return (
+                            <NavItem key={i} active={isRouteActive(item.path)}>
+                                <NavLink tag={Link} to={item.path}>
+                                    <FontAwesomeIcon icon={item.icon} fixedWidth />
+                                    <span>{item.name}</span>
+                                </NavLink>
+                            </NavItem>
+                        );
+                    } else if (item.subMenu) {
+                        return (
+                            <UncontrolledDropdown
+                                key={i}
+                                active={isRouteActive(getSubMenuRoutes(item))}
+                                nav
+                                inNavbar>
+                                <DropdownToggle nav>
+                                    <FontAwesomeIcon icon={item.icon} fixedWidth />
+                                    <span>{item.name}</span>
+                                </DropdownToggle>
+                                <DropdownMenu>
+                                    {item.subMenu.map((item, i) => {
+                                        if (item.heading) {
+                                            return (
+                                                <DropdownItem key={i} tag="h6" header>
+                                                    {item.heading}
+                                                </DropdownItem>
+                                            );
+                                        }
+
                                         return (
-                                            <DropdownItem key={i} tag="h6" header>
-                                                {item.heading}
+                                            <DropdownItem
+                                                key={i}
+                                                active={isRouteActive(item.path)}
+                                                tag={Link}
+                                                to={item.path}>
+                                                {item.name}
                                             </DropdownItem>
                                         );
-                                    }
+                                    })}
+                                </DropdownMenu>
+                            </UncontrolledDropdown>
+                        );
+                    }
 
-                                    return (
-                                        <DropdownItem key={i} tag={Link} to={item.path}>
-                                            {item.name}
-                                        </DropdownItem>
-                                    );
-                                })}
-                            </DropdownMenu>
-                        </UncontrolledDropdown>
-                    );
-                }
-
-                return null;
-            })}
-        </Nav>
-    </div>
-);
+                    return null;
+                })}
+            </Nav>
+        </div>
+    );
+};
 
 const mapStateToProps = store => ({ ...store.settings });
 
-export default connect(mapStateToProps)(Sidebar);
+export default connect(mapStateToProps)(withRouter(Sidebar));
