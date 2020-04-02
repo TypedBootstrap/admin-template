eb277b03437c839f17029000810f35221dc4dc70
Andrew Dyer <andrewdyer@outlook.com>
2020-04-02 15:38:55 +0100

    :sparkles: Added transitions when changing page

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,4 +1,5 @@
 import React, { Suspense } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import Page from 'components/Page';
 import PageLoader from 'components/PageLoader';
 import { Redirect, Route, Switch, withRouter } from 'react-router-dom';
@@ -7,22 +8,36 @@ import routes from 'config/routes';
 /**
  * Routes
  */
-const Routes = ({ location }) => (
-    <Page>
-        <Suspense fallback={<PageLoader />}>
-            <Switch location={location}>
-                {routes.map(route => (
-                    <Route
-                        key={route.key}
-                        component={route.component}
-                        path={route.path}
-                        exact={route.exact}
-                    />
-                ))}
-                <Redirect to="/" />
-            </Switch>
-        </Suspense>
-    </Page>
-);
+const Routes = ({ location }) => {
+    const currentLocation = location.pathname.split('/')[1] || '/';
+
+    return (
+        <Page>
+            <TransitionGroup>
+                <CSSTransition
+                    key={currentLocation}
+                    classNames="fade-in"
+                    exit={false}
+                    timeout={{ enter: 500, exit: 500 }}>
+                    <div>
+                        <Suspense fallback={<PageLoader />}>
+                            <Switch location={location}>
+                                {routes.map(route => (
+                                    <Route
+                                        key={route.key}
+                                        component={route.component}
+                                        path={route.path}
+                                        exact={route.exact}
+                                    />
+                                ))}
+                                <Redirect to="/" />
+                            </Switch>
+                        </Suspense>
+                    </div>
+                </CSSTransition>
+            </TransitionGroup>
+        </Page>
+    );
+};
 
 export default withRouter(Routes);
