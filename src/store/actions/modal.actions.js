98d8ececff601cf28be53dc9e6f827e0e34c8f94
Andrew Dyer <andrewdyer@outlook.com>
2021-02-11 13:08:26 +0000

    :hammer: Updated actions

--- src/store/actions/modal.actions.js
+++ src/store/actions/modal.actions.js
@@ -1,4 +1,4 @@
-import { HIDE_MODAL, OPEN_MODAL } from 'config/actions';
+import { HIDE_MODAL, OPEN_MODAL } from 'constants/actions';
 
 /**
  * Open a modal by type
--- src/store/actions/settings.actions.js
+++ src/store/actions/settings.actions.js
@@ -1,4 +1,4 @@
-import { CHANGE_SETTING, TOGGLE_SETTING } from 'config/actions';
+import { CHANGE_SETTING, TOGGLE_SETTING } from 'constants/actions';
 
 /**
  * Change a setting value
