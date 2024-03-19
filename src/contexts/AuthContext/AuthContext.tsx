49e1c75b1b3c1a59889d899009d915bad8790a6a
andrewdyer <andrewdyer@outlook.com>
2024-03-19 11:41:26 +0000

    feat: :sparkles: created auth context

---
+++ src/contexts/AuthContext/AuthContext.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+interface AuthContextProps {
+    user: any;
+    login: (user: any, callback: VoidFunction) => void;
+    logout: (callback: VoidFunction) => void;
+}
+
+export const AuthContext = React.createContext<AuthContextProps>(null!);
+
+interface AuthProviderProps {
+    children: React.ReactNode;
+}
+
+export const AuthProvider: React.FC<AuthProviderProps> = props => {
+    const [user, setUser] = React.useState<any>(null);
+
+    const login = (user: any, callback: VoidFunction): void => {
+        setUser(user);
+        callback();
+    };
+
+    const logout = (callback: VoidFunction): void => {
+        setUser(null);
+        callback();
+    };
+
+    return <AuthContext.Provider value={{ user, login, logout }}>{props.children}</AuthContext.Provider>;
+};
