a31909df542d473e9d3d169e6fcc37a8ab4225f4
andrewdyer <andrewdyer@outlook.com>
2024-03-25 15:40:47 +0000

    refactor: 🔨 refactored page to use default template

--- src/components/pages/HomePage/HomePage.tsx
+++ src/components/pages/HomePage/HomePage.tsx
@@ -1,73 +1,61 @@
 import React from 'react';
-import { DashboardLayout } from '@typedbootstrap/admin-components';
-import { Footer, Sidenav, Topnav, Widget } from 'components/organisms';
-import { useAppSelector } from 'hooks';
+import { Widget } from 'components/organisms';
+import { DefaultTemplate } from 'components/templates';
 import { Col, Container, Row } from 'reactstrap';
-import { selectSidebarToggled } from 'store/uiSlice';
 
 export interface HomePageProps {}
 
-const HomePage: React.FC<HomePageProps> = () => {
-    const sidebarToggled = useAppSelector(selectSidebarToggled);
-
-    return (
-        <DashboardLayout
-            topnav={<Topnav />}
-            sidenav={<Sidenav />}
-            sidenavToggled={sidebarToggled}
-            footer={<Footer />}>
-            <main>
-                <Container className="px-4" fluid>
-                    <h1 className="mt-4">Dashboard</h1>
-                    <ol className="breadcrumb mb-4">
-                        <li className="breadcrumb-item active">Dashboard</li>
-                    </ol>
-                    <Row>
-                        <Col xl="3" md="6">
-                            <Widget
-                                bg="primary"
-                                className="mb-4"
-                                linkLabel="View Details"
-                                linkTo="/"
-                                text="white">
-                                Primary Card
-                            </Widget>
-                        </Col>
-                        <Col xl="3" md="6">
-                            <Widget
-                                bg="warning"
-                                className="mb-4"
-                                linkLabel="View Details"
-                                linkTo="/"
-                                text="white">
-                                Warning Card
-                            </Widget>
-                        </Col>
-                        <Col xl="3" md="6">
-                            <Widget
-                                className="mb-4"
-                                bg="success"
-                                linkLabel="View Details"
-                                linkTo="/"
-                                text="white">
-                                Success Card
-                            </Widget>
-                        </Col>
-                        <Col xl="3" md="6">
-                            <Widget
-                                className="mb-4"
-                                bg="danger"
-                                linkLabel="View Details"
-                                linkTo="/"
-                                text="white">
-                                Danger Card
-                            </Widget>
-                        </Col>
-                    </Row>
-                </Container>
-            </main>
-        </DashboardLayout>
-    );
-};
+const HomePage: React.FC<HomePageProps> = () => (
+    <DefaultTemplate>
+        <Container className="px-4" fluid>
+            <h1 className="mt-4">Dashboard</h1>
+            <ol className="breadcrumb mb-4">
+                <li className="breadcrumb-item active">Dashboard</li>
+            </ol>
+            <Row>
+                <Col xl="3" md="6">
+                    <Widget
+                        bg="primary"
+                        className="mb-4"
+                        linkLabel="View Details"
+                        linkTo="/"
+                        text="white">
+                        Primary Card
+                    </Widget>
+                </Col>
+                <Col xl="3" md="6">
+                    <Widget
+                        bg="warning"
+                        className="mb-4"
+                        linkLabel="View Details"
+                        linkTo="/"
+                        text="white">
+                        Warning Card
+                    </Widget>
+                </Col>
+                <Col xl="3" md="6">
+                    <Widget
+                        className="mb-4"
+                        bg="success"
+                        linkLabel="View Details"
+                        linkTo="/"
+                        text="white">
+                        Success Card
+                    </Widget>
+                </Col>
+                <Col xl="3" md="6">
+                    <Widget
+                        className="mb-4"
+                        bg="danger"
+                        linkLabel="View Details"
+                        linkTo="/"
+                        text="white">
+                        Danger Card
+                    </Widget>
+                </Col>
+            </Row>
+        </Container>
+    </DefaultTemplate>
+);
 
 export default HomePage;
---
+++ src/components/templates/index.ts
@@ -0,0 +1,3 @@
+// Default Template
+export * from './DefaultTemplate';
+export { default as DefaultTemplate } from './DefaultTemplate';
